3eb8d5b008629f36189e9afc0bc613f0d6870468
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2016-11-14 15:21:51 +0100

    Create plateau.c

---
+++ plateau.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#define N 17
+
+typedef enum {vide,joueur1,joueur2,joueur3,joueur4,invalide}t_joueur;
+typedef enum {vide,pion,dame}t_piece;
+typedef enum {equipe1=1,equipe2}t_equipe;
+typedef struct {t_joueur joueur;t_piece piece;t_equipe equipe}t_case;
+typedef struct {char nom[20]; int pions_pris; int prions_perdus;int nb_coups}t_stats;
+
+t_case plateau[N][N];
+
