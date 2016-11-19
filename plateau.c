b62e8b0acdd8a3415e486196ed8d6709519b6346
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2016-11-19 01:49:15 +0100

    Update plateau.c

--- plateau.c
+++ plateau.c
@@ -1,3 +1,7 @@
+
+
+
+
 #include <stdio.h> 
 #include <stdlib.h>
 #define Z 17
@@ -13,22 +17,27 @@ typedef struct {t_coordonnees coordonnees; t_contenu contenu;} t_case;
 typedef struct {char nom[20]; int pions_pris; int pions_perdus; int nb_coup;} t_stats;
 t_contenu plateau[Z][Z] ;
 
+//fonction permettant de changer la valeur joueur d'une case
 void ChangeJoueur(int l, int c,t_contenu plateau[Z][Z],t_joueur nouv){
   plateau[l][c].joueur=nouv;
 }
+//fonction qui renvoie la valeur joueur de la case aux coordonnées saisies
 int litJoueur(int l,int c){
     return(plateau[l][c].joueur);
 }
-       
+//Ajoute à la liste la case aux coordonnées l,c       
 void DeplaAjout(int l, int c,t_liste* ls_coup_d){
-   t_case cell_dispo;
+   t_case cell_dispo; // creation d'un case à empiler
+  //assignation des infos de la cellule à celle qui va empiler
    cell_dispo.coordonnees.x=c;
    cell_dispo.coordonnees.y=l;
-   cell_dispo.joueur=vide;
+   cell_dispo.contenu.joueur=plateau[l][c].joueur;
+   cell_dispo.contenu.piece=plateau[l][c].piece;
    if(plateau[l][c].joueur==1 || plateau[l][c].joueur==3 ) cell_dispo.equipe=1;
    if(plateau[l][c].joueur==2 || plateau[l][c].joueur==4 ) cell_dispo.equipe=2;
-   ajout_droit(ls_coup_d,cell_dispo);
+   ajout_droit(ls_coup_d,cell_dispo); // ajout dans la liste de la case
 } 
+//fonction servant à lister les emplacements ou un pion peut se deplacer
 int dispoPion(t_coordonnees coor,t_liste *ls_coup_d){
   int l,c,coup_dispo;
   l=coor.y;
@@ -37,55 +46,81 @@ int dispoPion(t_coordonnees coor,t_liste *ls_coup_d){
    // cas des deplacements pour Pion du joueur 
     if(l+c>16 && (c<=l)){ // si la reference est le triangle de depart du joueur
       if(litJoueur(l-1,c-1)==0){ 
-         DeplaAjout(j,l,c,ls_coup_d);
-         coup_dispo=1;
+         DeplaAjout(l,c,ls_coup_d); // ajout de la case dans la liste des coups dispo
+         coup_dispo=1; // donne 1 a la valeur des coup_dispos, designant la piece comme jouable
        }
-       if(litjoueur(l-1,c+1)==0){
-         DeplaAjout(j,l,c,ls_coup_d);
+       if(litjoueur(l-1,c+1)==0){ // même schema que la boucle precedante
+         DeplaAjout(l,c,ls_coup_d);
          coup_dispo=1;
        } 
     }
     else if(l+c<16 && (c<=l){//si la reference est le triangle de gauche
        if(litJoueur(l-1,c-1)==0){
-         DeplaAjout(j,l,c,ls_coup_d);
+         DeplaAjout(l,c,ls_coup_d); 
          coup_dispo=1;
        }
        if(litJoueur(l+1,c-1)==0){
-         DeplaAjout(j,l,c,ls_coup_d);
+         DeplaAjout(l,c,ls_coup_d);
          coup_dispo=1;
        }
-   else if(l+c>16 &&(c>=l){//si la reference est le triangle de droite
+    }
+    else if(l+c>16 &&(c>=l){//si la reference est le triangle de droite
       if(litJoueur(l+1,c+1)==0){
-         DeplaAjout(j,l,c,ls_coup_d);
+         DeplaAjout(l,c,ls_coup_d);
          coup_dispo=1;
        }
      if(litJoueur(l-1,c+1)==0){
-         DeplaAjout(j,l,c,ls_coup_d);
+         DeplaAjout(l,c,ls_coup_d);
          coup_dispo=1;
        }
    }
-  return coup_dispo;
+  return coup_dispo; //fin de la fonction, delivre vrai si coup dispo
 }
-   
+
 int dispoDame(t_coordonnees coor,t_liste *ls_coup_d){
   int l,c,coup_dispo,fin;
   l=coor.y;
   c=coor.c;
   
    // cas des deplacements pour Dame du joueur 
-  for (c=c+1,l=l+1;plateau[l][c].joueur==0;l++,c++){
-    DeplaAjout(j,l,c,ls_coup_d);
-           
+  for(c=c+1,l=l+1;plateau[l][c].joueur==0;l++,c++){ // Traitement des differentes diagonales
+    DeplaAjout(l,c,ls_coup_d);// Ajout des cases vides de la diagonale jusqu'a la rencontre d'une piece
+    coup_dispo=1; // Piece pouvant etre jouée
+  } 
+  for(c=c-1,l=l+1;plateau[l][c].joueur==0;l++,c--){// même schéma
+    DeplaAjout(l,c,ls_coup_d);
+    coup_dispo=1; 
+  }
+  for(c=c+1,l=l-1;plateau[l][c].joueur==0;l--,c++){
+    DeplaAjout(l,c,ls_coup_d);
+    coup_dispo=1; 
+  }
+  for(c=c-1,l=l-1;plateau[l][c].joueur==0;l--,c--){
+    DeplaAjout(l,c,ls_coup_d);
+    coup_dispo=1; 
+  }
+  return coup_dispo;
+}
 /*____________________________________________________________________________________________________________________________*/
-int coupDispo(t_coordonnees coor,t_joueur j/*eviter deplacer pion adverse*/,t_liste* ls_coup_d);
+/*--------------------------------------- FONCTIONS MAJEURES----------------------------------------------------------------- */
+/*____________________________________________________________________________________________________________________________*/
+           
+           
+ /* fonction prenant en coordonnées le pion à jouer. 
+Retourne vrai si le joueur peut jouer le pion, et fournit une liste avec les deplacement possibles*/
+int coupDispo(t_coordonnees coor,t_joueur j/*pour eviter de deplacer un pion adverse*/,t_liste* ls_coup_d);
   int coup_dispo,l,c;
   l=coor.y;
   c=coor.c;
   vider_liste(ls_coup_d);// on vide la liste 
   en_tete(ls_coup_d); // on se place en_tete de liste
-  if(plateau[l][c].piece==1) coup_dispo=dispoPion(coor,ls_coup_d); // si la piece est un pion, calcul de ses deplacements
-  if(plateau[l][c].piece==2) coup_dispo=dispoDame(coor,ls_coup_d);
+  if(plateau[l][c].joueur==j){ // verification que la piece choisie appartien au joueur 
+    if(plateau[l][c].piece==1) coup_dispo=dispoPion(coor,ls_coup_d); // si la piece est un pion, calcul de ses deplacements
+    if(plateau[l][c].piece==2) coup_dispo=dispoDame(coor,ls_coup_d); // si la piece est une dame, calcul de ses deplacements
+  }
+  return coup_dispo; //renvoi vrai si la piece est jouable
 }
+           
 void init(t_contenu plateau[Z][Z]){
   int l,c;
   for(l=0,c=0;l<Z;l++){
