cef3bce1c352f908e7ca1ceda8079a5f01ac08ba
William Berard <berardwilliam@gmail.com>
2016-11-22 15:24:52 +0100

    Update jeu.h

--- include/jeu.h
+++ include/jeu.h
@@ -5,13 +5,13 @@ int verifierdefaite();
 int peutPrendrepion(t_coordonnees coord, t_joueur joueur,t_liste* ls_coup_d);
 int peutPrendredame(t_coordonnees coord, t_joueur joueur,t_liste* ls_coup_d);
 int PeutPrendre(t_coordonnees coord, t_joueur joueur,t_liste* ls_coup_d);
-int deppossible(t_joueur joueur);
+int depPossible(t_joueur joueur);
 int partieFinie(int joueur);
-void afficherstats();
-void reinitstatsjoueur(t_joueur joueur);
-void reinitstats();
-void statpionpris(t_joueur joueur);
-void statpionperdu(t_joueur joueur);
-void statdep(t_joueur joueur);
+void afficherStats();
+void reinitStatsjoueur(t_joueur joueur);
+void reinitStats();
+void statPionpris(t_joueur joueur);
+void statPionperdu(t_joueur joueur);
+void statDep(t_joueur joueur);
 
 #endif
