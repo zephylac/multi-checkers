bc6614abc26516cbc23b2bac3b5446a622380726
William Berard <berardwilliam@gmail.com>
2016-11-22 16:29:36 +0100

    Update jeu.h

--- include/jeu.h
+++ include/jeu.h
@@ -8,6 +8,7 @@ int PeutPrendre(t_coordonnees coord, t_joueur joueur,t_liste* ls_coup_d);
 int depPossible(t_joueur joueur);
 int partieFinie(int joueur);
 void afficherStats();
+void finPartie(int joueur);
 void reinitStatsjoueur(t_joueur joueur);
 void reinitStats();
 void statPionpris(t_joueur joueur);
