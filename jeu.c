3bd12f772bb64d3bc21aaa8ef37cab69e45e77d6
zephylac <spi2163@ic2s122-16.univ-lemans.fr>
2016-11-28 11:50:21 +0100

    Suppresion d'un vider liste inutile

--- jeu.c
+++ jeu.c
@@ -142,7 +142,6 @@ int peutPrendredame(t_coordonnees coord, t_joueur joueur,t_liste* ls_coup_d){
 }
 
 int peutPrendre(t_coordonnees coord, t_joueur joueur,t_liste* ls_coup_d){
-	vider_liste(ls_coup_d);
 	int prise = 0;
 	t_case nouv;	//nouvel élément à insérer dans la liste
 	t_coordonnees nouvcoord;
