2aa0287d57f1f28f40fa3e21e5a630fcc0bc583f
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2016-11-28 11:01:50 +0100

    Update plateau.c

--- plateau.c
+++ plateau.c
@@ -11,11 +11,11 @@ void ChangeJoueur(int l, int c,t_joueur nouv){
   plateau[l][c].joueur=nouv;
 }
 //fonction qui renvoie la valeur joueur de la case aux coordonnées saisies
-int litJoueur(int l,int c){
+t_joueur litJoueur(int l,int c){
     return(plateau[l][c].joueur);
 }
 //Ajoute à la liste la case aux coordonnées l,c       
-void DeplaAjout(int l, int c,t_liste ls_coup){
+void DeplaAjout(int l, int c,t_liste *ls_coup){
    t_case cell_dispo; // creation d'un case à empiler
   //assignation des infos de la cellule à celle qui va empiler
    cell_dispo.coordonnees.x=c;
@@ -24,13 +24,12 @@ void DeplaAjout(int l, int c,t_liste ls_coup){
    cell_dispo.contenu.piece=plateau[l][c].piece;
    if(plateau[l][c].joueur==1 || plateau[l][c].joueur==3 ) cell_dispo.contenu.equipe=1;
    if(plateau[l][c].joueur==2 || plateau[l][c].joueur==4 ) cell_dispo.contenu.equipe=2;
-   ajout_droit(&ls_coup,cell_dispo); // ajout dans la liste de la case
+   ajout_droit(ls_coup,cell_dispo); // ajout dans la liste de la case
 } 
 
 //fonction servant à lister les emplacements ou un pion peut se deplacer
-int dispoPion(t_coordonnees coor,t_liste ls_coup_d){
+int dispoPion(t_coordonnees coor,t_liste *ls_coup_d){
   int l,c,coup_dispo;
-  vider_liste(&ls_coup_d);
   l=coor.y;
   c=coor.x;
   
@@ -68,9 +67,8 @@ int dispoPion(t_coordonnees coor,t_liste ls_coup_d){
   return coup_dispo; //fin de la fonction, delivre vrai si coup dispo
 }
 
-int dispoDame(t_coordonnees coor,t_liste ls_coup_d){
+int dispoDame(t_coordonnees coor,t_liste *ls_coup_d){
   int l,c,coup_dispo,fin;
-  vider_liste(&ls_coup_d);
   l=coor.y;
   c=coor.x;
   
@@ -118,17 +116,43 @@ void InitCase(int l,int c,t_joueur j){
 /*____________________________________________________________________________________________________________________________*/
            
            
-            
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
 void init(){
   int l,c;
   for(l=0,c=0;l<Z;l++){
     for(c=0;c<Z;c++){ // parcours une seule fois la matrice
       if((c<4 && (l<4 || l>=Z-4))||(c>=Z-4 && (l<4 || l>=Z-4))) ChangeJoueur(l,c,invalide); // case dans les coins sont invalides
       else if((l+c)%2==0) ChangeJoueur(l,c,vide);//indique que la case est vide
-      else if((c>=4 && c<Z-4 ) && l>=Z-5 && (l+c)%2==1) InitCase(l,c,joueur1);//place les pions du joueur 1
-      else if(c>=Z-5 && (l>=4 && l<Z-4) && (l+c)%2==1) InitCase(l,c,joueur2);//place les pions du joueur 2
-      else if((c>=4 && c<Z-4) && l<=4 && (l+c)%2==1) InitCase(l,c,joueur3);//place les pions du joueur 3
-      else if(c<=4 &&(l>=4 && l<Z-4) && (l+c)%2==1) InitCase(l,c,joueur4);//place les pions du joueur 4  
+      else if(c>=4 && c<Z-4 && l>=Z-5 && (l+c)%2==1) InitCase(l,c,joueur1);//place les pions du joueur 1
+      else if(c>=Z-5 && l>=4 && l<Z-4 && (l+c)%2==1) InitCase(l,c,joueur2);//place les pions du joueur 2
+      else if(c>=4 && c<Z-4 && l<=4 && (l+c)%2==1) InitCase(l,c,joueur3);//place les pions du joueur 3
+      else if(c<=4 && l>=4 && l<Z-4 && (l+c)%2==1) InitCase(l,c,joueur4);//place les pions du joueur 4  
     }
   }
 }
@@ -136,8 +160,22 @@ void init(){
 
 void afficher(){
   system("clear");
-  int l,c,joueur;
+  char carac = 'A';
+  int i=0,l,c,joueur;
+  printf("  ");
   for(l=0,c=0;l<Z;l++){
+	  printf(" %c ",carac);
+	  carac++;
+  }
+  printf("\n");
+  for(l=0,c=0;l<Z;l++){
+    if(i <10){
+      printf("%i |",i);
+    }
+    else{
+      printf("%i|",i);
+    }
+    i++;
     for(c=0;c<Z;c++){ // parcours une seule fois la matrice
       joueur=litJoueur(l,c);
       if(joueur==5) printf("   ");
@@ -152,18 +190,16 @@ void afficher(){
   }
 }  
 
-int coupForce(t_joueur j,t_liste ls_coup_f /* type t_case*/){ // retourne 1 si coup forcé ou 0 si pas coup forcé          
+int coupForce(t_joueur j,t_liste *ls_coup_f /* type t_case*/){ // retourne 1 si coup forcé ou 0 si pas coup forcé          
   int l,c,coup_For;
   t_coordonnees cell_pos;
-  t_joueur j_Pos;
-  vider_liste(&ls_coup_f);
+  vider_liste(ls_coup_f);
   coup_For=0;
-  for(l=0;l<N;l++){
-    for(c=0;c<N;c++){
+  for(l=0;l<Z;l++){
+    for(c=0;c<Z;c++){
       cell_pos.x=c;
       cell_pos.y=l;
-      j_Pos=litJoueur(l,c);
-      if(j==j_Pos && peutPrendre(cell_pos,j,&ls_coup_f) ){ //-----------------------a completer avec fonction willi--------------------
+      if(j==litJoueur(l,c) && peutPrendre(cell_pos,j,ls_coup_f) ){ //-----------------------a completer avec fonction willi--------------------
         DeplaAjout(l,c,ls_coup_f);
         coup_For=1;
       }
@@ -180,8 +216,8 @@ int coupDispo(t_coordonnees coor,t_joueur j/*pour eviter de deplacer un pion adv
   vider_liste(ls_coup_d);// on vide la liste 
   en_tete(ls_coup_d); // on se place en_tete de liste
   if(plateau[l][c].joueur==j){ // verification que la piece choisie appartien au joueur 
-    if(plateau[l][c].piece==1) coup_dispo=dispoPion(coor,*ls_coup_d); // si la piece est un pion, calcul de ses deplacements
-    if(plateau[l][c].piece==2) coup_dispo=dispoDame(coor,*ls_coup_d); // si la piece est une dame, calcul de ses deplacements
+    if(plateau[l][c].piece==1) coup_dispo=dispoPion(coor,ls_coup_d); // si la piece est un pion, calcul de ses deplacements
+    if(plateau[l][c].piece==2) coup_dispo=dispoDame(coor,ls_coup_d); // si la piece est une dame, calcul de ses deplacements
   }
   return coup_dispo; //renvoi vrai si la piece est jouable
 }
@@ -227,11 +263,11 @@ void prendrePiece(t_coordonnees dep,t_coordonnees arriv){
 void switchCoord(int x, int y){
   t_contenu inter;
   inter = plateau[x][y];
-  plateau[x][y] = plateau[y][Z-1-x];
-  plateau[y][Z-1-x] = plateau[Z-1-x][Z-1-y];
-  plateau[Z-1-x][Z-1-y] = plateau[Z-1-y][x];
-  plateau[Z-1-y][x] = inter;
-}
+  plateau[x][y] = plateau[Z-1-y][x];
+  plateau[Z-1-y][x] = plateau[Z-1-x][Z-1-y];
+  plateau[Z-1-x][Z-1-y] = plateau[y][Z-1-x];
+  plateau[y][Z-1-x] = inter;
+  }
 
 void tourner(){
   int i,j;
@@ -256,3 +292,4 @@ void tourner(){
   j = 9;
   switchCoord( i, j);
 }
+
