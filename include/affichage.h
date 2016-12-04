b38de8675d7ef6ea107766bfa19324b9f4cae47e
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2016-12-04 16:14:22 +0100

    Create affichage.h

---
+++ include/affichage.h
@@ -0,0 +1,13 @@
+#ifndef H_GL_AFFICHAGE
+#define H_GL_AFFICHAGE
+#include "struct.h"
+
+void lignePleine();
+void ligneVide();
+void margeGauche(int larg_plat);
+void margeDroite(int larg_plat);
+void margeHaut();
+void margeBas();
+void afficher();
+
+#endif
