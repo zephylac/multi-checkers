383a685505d755daf92efb62b3c1e3b76e42b77f
zephylac <spi2163@ic2s114-09.univ-lemans.fr>
2016-12-05 10:09:44 +0100

    Ajout du système d esauvegarde

--- fichier.c
+++ fichier.c
@@ -2,17 +2,20 @@
 #include <string.h>
 #include "include/struct.h"
 
-void sauv(t_joueur joueur){
+void sauv(){
 	FILE * fic;
 	int i, j;
 	char nom[30];
-
+	
+	printf("\nLe nom de la sauvegard ene doit pa sdépasser 30 charactères");
 	printf("\nEntrer le nom de la sauvegarde : ");
 	scanf("%s",&nom);
 	
 	fic = fopen(nom,"w");
 
-	fprintf(fic,"%i ",joueur);
+	//Seulement utile lorsque l'on sauvegarde au milieu d'un tour, lorsque l tour ne sera pas au joueur 1
+	//Il faudra init la variable tour joueur 
+	//fprintf(fic,"%i ",joueur);
 	
 	for(i = 0; i < Z; i++){
 		for( j = 0; j < Z; j++){
@@ -22,7 +25,7 @@ void sauv(t_joueur joueur){
 	fclose(fic);
 }
 
-t_joueur lireSauv(){
+void lireSauv(){
 	FILE * fic;
 	int i, j;
 	char nom[30];
@@ -30,17 +33,20 @@ t_joueur lireSauv(){
 	printf("\nEntrer le nom de la sauvegarde à lire");
 	scanf("%s",&nom);
 	fic = fopen(nom,"r");
-	while(
+	while(fic == NULL){
+		printf("Erreur, le nom entré n'est pas valide\n");
+		printf("\nEntrer le nom de la sauvegarde à lire");
+		scanf("%s",&nom);
+		fic = fopen(nom,"r");
+	}	
+	
+	//Seulement utile lorsque l'on sauvegarde au milieu d'un tour, lorsque l tour ne sera pas au joueur 1
+	//Il faudra init la variable tour joueur 
+	//fscanf(fic,"%i ",&joueur);	
 
-	fic = fopen(save.txt,"r");
-	t_joueur joueur;
-
-	fscanf(fic,"%i ",&joueur);
 	for(i = 0; i < Z; i++){
 		for( j = 0; j < Z; j++){
 			fscanf(fic,"%i %i %i ",plateau[i][j].joueur, plateau[i][j].piece,plateau[i][j].equipe);
 		}
 	}
-	return joueur;
 }
-
---
+++ include/fichier.h
@@ -0,0 +1,6 @@
+#ifndef H_GL_FICHIER
+#define H_GL_FICHIER
+#include "struct.h"
+
+void sauv()
+void lireSauv()
