064dff8f63177d58dbac7b465c36ba58174a9878
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2016-12-05 09:59:07 +0100

    Update multi-dames.c

--- multi-dames.c
+++ multi-dames.c
@@ -4,9 +4,18 @@
 #include "include/joueur.h"
 #include "include/liste_ptr.h"
 int main(){
+	int choix=menu();
 	init_liste(&ls_coup_dep);
 	init_liste(&ls_coup_arr);
-	init();
-	deroulementPartie();
+	if(choix==1){
+		init();
+		deroulementPartie();
+	}
+	if (choix==2){
+	}
+	if (choix==3){
+	}
+	if (choix==4){
+	}
 return 1;
 }
