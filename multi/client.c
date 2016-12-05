e936fc775daa45eb275b4c1e231c46de7aa44281
zephylac <spi2163@ic2s114-09.univ-lemans.fr>
2016-12-05 10:11:50 +0100

    Support d'une partie multijoueur

--- multi/client.c
+++ multi/client.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-char file ="partie.txt";
+char file[30] ="partie.txt";
 
 fpos_t placerLigne(int ligne){
 	FILE * fic;
@@ -11,8 +11,8 @@ fpos_t placerLigne(int ligne){
 	fpos_t position;
 	rewind(fic);
 	while(i < ligne){
-		fscanf(file,"%c",&carac);
-		if(carac == "\n"){
+		fscanf(fic,"%c",&carac);
+	if(carac == '\n'){
 			i++;
 		}
 	}
@@ -22,18 +22,19 @@ fpos_t placerLigne(int ligne){
 }
 
 void changerStatus(int joueur){
+	FILE * fic;
 	fpos_t position;
 	int present;
-	placerLigne(joueur); 
+	position = placerLigne(joueur); 
 	fic = fopen(file,"r+");
 	fsetpos(fic,&position);
 	fscanf(fic,"joueur_present : %i",&present);
 	fsetpos(fic,&position);	
 	if(present){
-		fprintf("joueur_present : 0\n");	
+		fprintf(fic,"joueur_present : 0\n");	
 	}
 	else{
-		fprintf("joueur_present : 1\n");
+		fprintf(fic,"joueur_present : 1\n");
 	}
 	fclose(fic);
 }
@@ -44,46 +45,60 @@ void quitterPartie(int joueur){
 }
 
 int placerJoueur(){
+	FILE * fic;
 	int joueur,present;
 	fpos_t position;
 	fic = fopen(file,"r");
 	position = placerLigne(1);
 	fsetpos(fic,&position);
-	fscanf(fic,"joueur_present : %i",&present); 
-	if(present){
+	fscanf(fic,"joueur_present : %i\n",&present); 
+	if(!present){
 		changerStatus(1);
 		joueur = 1;
-	}
-	fscanf(fic,"joueur_present : %i",&present); 
-	else if(present){
-		changerStatus(2);
-		joueur = 2;
-	}
-	fscanf(fic,"joueur_present : %i",&present); 
-	else if(present){
-		changerStatus(3);
-		joueur = 3;
-	}
-	fscanf(fic,"joueur_present : %i",&present); 
-	else if(present){
-		changerStatus(4);
-		joueur = 4;
+		
 	}
 	else{
-		printf("La partie est pleine\n");
-		joueur = 0;
+		fscanf(fic,"joueur_present : %i\n",&present); 
+		if(!present){
+			changerStatus(2);
+			joueur = 2;
+		}
+		else{
+			fscanf(fic,"joueur_present : %i\n",&present); 
+			if(!present){
+				changerStatus(3);
+				joueur = 3;
+			}
+			else{
+				fscanf(fic,"joueur_present : %i\n",&present); 
+				if(!present){
+					changerStatus(4);
+					joueur = 4;
+				}
+				else{
+					printf("La partie est pleine\n");
+					joueur = 0;
+				}
+			}
+		}
 	}
+	fclose(fic);
 	return joueur;
 }
 void attendreTour(int joueur){
 	FILE * fic;
 	int tour;
-	fic = fopen("tour.txt","r");
-	fscanf(fic,"tour_joueur : %i",tour);
-	while(tour != joueur){}
+	fic = fopen(file,"r");
+	fscanf(fic,"tour_joueur : %i",&tour);
+	while(tour != joueur){
+		rewind(fic);
+		fscanf(fic,"tour_joueur : %i",&tour);
+	}
+	fclose(fic);
 }
 
 void joueurSuivant(){
+	FILE * fic;
 	int joueur;
 	fic = fopen(file,"r+");
 	fscanf(fic,"tour_joueur : %i\n",&joueur);
@@ -102,11 +117,11 @@ void main(){
 		printf("Bienvenue, vous jouez en tant que le joueur %i\n",joueur);
 	}
 	else{
-		while(!finPartie()){
+	//	while(!finPartie()){
 			attendreTour(joueur);
-			jouertour(tour);
+		//	jouertour(joueur);
 			joueurSuivant();
-		}
+	//	}
 	}
 //	quitterPartie(1);
 }
