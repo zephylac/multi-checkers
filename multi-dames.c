514cf6191402800b722d9c41a926a16a5b8cc660
zephylac <spi2163@ic2s122-16.univ-lemans.fr>
2016-12-09 09:45:41 +0100

    Ajout de la lecture des sauvegarde ainsi que l'écriture des fichiers

--- multi-dames.c
+++ multi-dames.c
@@ -3,17 +3,26 @@
 #include "include/plateau.h"
 #include "include/joueur.h"
 #include "include/liste_ptr.h"
+#include "include/fichier.h"
+
 int main(){
+	char nom[30];
 	int choix=menu();
 	init_liste(&ls_coup_dep);
 	init_liste(&ls_coup_arr);
 	if(choix==1){
 		init();
+		printf("Veuillez choisir le nom de la sauvegarde");
+		scanf("%s",nom);
+		sauv(nom);
 		deroulementPartie();
 	}
 	if (choix==2){
+		//multi
 	}
 	if (choix==3){
+		lireSauv();
+		deroulementPartie();
 	}
 	if (choix==4){
 //		manuelDuJeu();
