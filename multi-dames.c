231f4d5685d6a50bb450a46d44c6d65bddcf4490
BrinonBat <baptiste.brinon.etu@univ-lemans.fr>
2016-12-15 16:41:38 +0100

    ajout d'une option pour quitter

--- multi-dames.c
+++ multi-dames.c
@@ -7,39 +7,44 @@
 #include "include/affichage.h"
 int main(){
 	FILE * fic;
-	char nom[30];
-	int choix=menu();
 	init_liste(&ls_coup_dep);
 	init_liste(&ls_coup_arr);
-	if(choix==1){
-		init();
-		printf("Veuillez choisir le nom de la sauvegarde: ");
-		scanf("%s",nom);
-		sauv(nom);
-		deroulementPartie(nom);
-	}
-	if (choix==2){
-		printf("Le mot de passe est \"dame\"");
-		sleep(1);
-		system("ssh dame@52.28.201.112");
-		//multi
-	}
-	if (choix==3){
-		printf("\nEntrer le nom de la sauvegarde à lire: ");
-		scanf("%s",nom);
-		fic = fopen(nom,"r");
-		while(fic == NULL){
-			printf("Erreur, le nom entré n'est pas valide\n");
-			printf("\nEntrer le nom de la sauvegarde à lire");
+	char nom[30];
+	int choix;
+	while(choix!=5){
+		choix=menu();
+		if(choix==1){
+			init();
+			printf("Veuillez choisir le nom de la sauvegarde: ");
+			scanf("%s",nom);
+			sauv(nom);
+			deroulementPartie(nom);
+		}
+		if (choix==2){
+
+			system("ssh dame@52.28.201.112");
+			//multi
+		}
+		if (choix==3){
+			printf("\nEntrer le nom de la sauvegarde à lire: ");
 			scanf("%s",nom);
 			fic = fopen(nom,"r");
+			while(fic == NULL){
+				printf("Erreur, le nom entré n'est pas valide\n");
+				printf("\nEntrer le nom de la sauvegarde à lire");
+				scanf("%s",nom);
+				fic = fopen(nom,"r");
+			}
+			fclose(fic);
+			lireSauv(nom);
+			deroulementPartie(nom);
 		}
-		fclose(fic);
-		lireSauv(nom);
-		deroulementPartie(nom);
+		if (choix==4){
+			manuelDuJeu();
+		}
+		if (choix==5){
+		printf("\n merci d'avoir joué! ");
+		return 1;
+		}
 	}
-	if (choix==4){
-		manuelDuJeu();
-	}
-return 1;
 }
