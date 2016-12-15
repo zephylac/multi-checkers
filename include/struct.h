eb51f431f2cba3c8b8f4491c3cb048bedd6e1f0d
zephylac <spi2163@ic2s114-10.univ-lemans.fr>
2016-12-15 11:54:30 +0100

    MAj doxygen

--- include/struct.h
+++ include/struct.h
@@ -1,3 +1,4 @@
+/*! \file struct.h */
 #ifndef H_GL_STRUCT
 #define H_GL_STRUCT
 
@@ -55,8 +56,8 @@ typedef enum {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q}t_lettre;
  * \brief Structure de coordonnées 
  */
 typedef struct {
-	int x;	/*< Valeur contenu sur l'axe x */
-       	int y;	/*< Valeur contenu sur l'axe y */ 
+	int x;	/*!< Valeur contenu sur l'axe x */
+       	int y;	/*!< Valeur contenu sur l'axe y */ 
 }
 t_coordonnees;
 
@@ -66,8 +67,8 @@ t_coordonnees;
  * \brief Structure de coordonnées de départ et d'arrivée
  */ 
 typedef struct {
-	t_coordonnees dep;	/*< Valeur contenant les coordonnées de départs */ 
-       	t_coordonnees arr;	/*< Valeur contenant les coordonnées de d'arrivées */
+	t_coordonnees dep;	/*!< Valeur contenant les coordonnées de départs */ 
+       	t_coordonnees arr;	/*!< Valeur contenant les coordonnées de d'arrivées */
 }				
 t_choix;
 
@@ -77,9 +78,9 @@ t_choix;
  * \brief Structure d'une case contenant un joueur, une pièce ainsi qu'une équipe
  */
 typedef struct {
-	t_joueur joueur;	/*< Valeur du joueur */
-       	t_piece piece;		/*< Valeur de la pièce */
-       	t_equipe equipe;	/*< Valeur de l'équipe */
+	t_joueur joueur;	/*!< Valeur du joueur */
+       	t_piece piece;		/*!< Valeur de la pièce */
+       	t_equipe equipe;	/*!< Valeur de l'équipe */
 }
 t_contenu;
 
@@ -89,8 +90,8 @@ t_contenu;
  * \brief Structure contenant les coordonnées d'une cases ainsi que sa valeur
  */
 typedef struct {
-	t_coordonnees coordonnees;	/*< Coordonnées de la case */
-       	t_contenu contenu;		/*< Valeur contenu dans la case */
+	t_coordonnees coordonnees;	/*!< Coordonnées de la case */
+       	t_contenu contenu;		/*!< Valeur contenu dans la case */
 }
 t_case;
 
@@ -100,9 +101,9 @@ t_case;
  * \brief Structure contenant les statistique d'un joueur
  */
 typedef struct {
-       	int pions_pris;		/*< Le nombre de pion pris */
-       	int pions_perdus;	/*< Le nombre de pion perdu */
-       	int nb_coup;		/*< Le nombre de coup effectué */
+       	int pions_pris;		/*!< Le nombre de pion pris */
+       	int pions_perdus;	/*!< Le nombre de pion perdu */
+       	int nb_coup;		/*!< Le nombre de coup effectué */
 }
 t_stats;
 
@@ -112,9 +113,9 @@ t_stats;
  * \brief Structure contenant une case ainsi que le lien pour connaître la suivante de même que la précédente.Utile pour l'utilisation d'une liste
  */
 typedef struct element{
-	t_case valeur;		/*< Coordonnées et contenu de la case */
-       	struct element * pred;	/*< Lien vers la case précédente */
-       	struct element * succ;	/*< Lien vers la case suivante */
+	t_case valeur;		/*!< Coordonnées et contenu de la case */
+       	struct element * pred;	/*!< Lien vers la case précédente */
+       	struct element * succ;	/*!< Lien vers la case suivante */
 }
 t_element;
 
@@ -124,8 +125,8 @@ t_element;
  * \brief Structure contenant le drapeau et l'élément courant. Utile pour l'utilisation d'une liste
  */
 typedef struct {
-	t_element * drapeau;	/*< Element du drapeau */
-	t_element * ec;		/*< Element de l'élément courant */
+	t_element * drapeau;	/*!< Element du drapeau */
+	t_element * ec;		/*!< Element de l'élément courant */
 }
 t_liste;
 
