2dc8a444e5100670082657abf1178b59c0d9e56e
William Berard <berardwilliam@gmail.com>
2017-01-01 19:25:04 +0100

    choix en fonction du menu SDL

--- multi-dames.c
+++ multi-dames.c
@@ -5,14 +5,12 @@
 #include "include/liste_ptr.h"
 #include "include/fichier.h"
 #include "include/affichage.h"
-int main(){
+int menu_choix(int choix){
 	FILE * fic;
 	init_liste(&ls_coup_dep);
 	init_liste(&ls_coup_arr);
 	char nom[30];
-	int choix;
 	while(choix!=5){
-		choix=menu();
 		if(choix==1){
 			init();
 			printf("Veuillez choisir le nom de la sauvegarde: ");
